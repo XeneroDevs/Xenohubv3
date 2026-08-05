c4a50a6abe67c0ec5c265b25320c6bf804605297
XeneroDevs <sarhanlome@gmail.com>
2026-08-05 23:54:30 +0300

    Updated Library.lua test

--- Library.lua
+++ Library.lua
@@ -1,7 +1,20 @@
 local Library = {}
 
-function Library.SomeFunction()
-    print("Hello")
+Library.ErrorPrinting = false
+
+function Library.BuildEmbed()
+    return {
+        Info = {}
+    }
+end
+
+function Library.ColorConverter(color)
+    return color
+end
+
+function Library:Send(data)
+    print("Send called")
+    print(data.url)
 end
 
 return Library
